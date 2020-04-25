42fb4e9c62a72181276c72c451013af112405b13
Mark Kvetny <m.kvetny@outlook.com>
2020-04-25 02:33:54 +0200

    pose callback refactor

--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -6,24 +6,23 @@ export default function Canvas() {
   const noseCanvasRef = useRef(null);
   const drawCanvasRef = useRef(null);
   let prevX, prevY;
+  let noseCtx, drawCtx;
 
   const [videoRef, { status, error }] = usePose((pose) => {
-    /* cancel if there's no nose */
-    if (pose.keypoints[0].score < 0.2) return;
-    /* get the nose position */
-    let { x, y } = pose.keypoints[0].position;
+    if (!noseCtx) noseCtx = noseCanvasRef.current.getContext("2d");
+    if (!drawCtx) drawCtx = noseCanvasRef.current.getContext("2d");
 
+    clear(noseCtx);
+
+    if (pose.keypoints[0].score < 0.2) return;
+
+    let { x, y } = pose.keypoints[0].position;
     if (prevX && prevY) {
       x = lerp(prevX, x, 0.5);
       y = lerp(prevY, y, 0.5);
     }
 
-    const noseCtx = noseCanvasRef.current.getContext("2d");
-    const drawCtx = noseCanvasRef.current.getContext("2d");
-
-    clear(noseCtx);
-    circle(noseCtx, x, y, 30, "cornflowerblue");
-
+    circle(noseCtx, x, y, 30, "tomato");
     prevX = x;
     prevY = y;
   });
