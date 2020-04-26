ff947f702ab5d1a83b5ad3ed7dc2a670a0860155
Mark Kvetny <m.kvetny@outlook.com>
2020-04-26 16:48:37 +0200

    image save and error states

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,12 +3,12 @@ import Canvas from "../src/components/canvas";
 export default function HomePage() {
   return (
     <div className="container mx-auto px-6 font-sans">
-      <header className="py-5 text-center mb-6">
+      <header className="py-5 text-center mb-10">
         <h1 className="text-4xl md:text-5xl mb-2 font-bold leading-tight">
-          Welcome to nosedraw!
+          Welcome to Nosedraw!
         </h1>
         <p className="text-2xl max-w-xs mx-auto md:max-w-full leading-tight">
-          the app that lets you draw with your nose
+          the website that lets you draw with your nose
         </p>
       </header>
       <div>
--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -53,80 +53,107 @@ export default function Canvas() {
   );
 
   return (
-    <div className="flex flex-col xl:flex-row max-w-2xl mx-auto xl:max-w-full">
-      <div className="canvas-container rounded overflow-hidden shadow-lg w-full xl:w-1/2 mb-12 xl:mb-0">
-        <canvas ref={drawCanvasRef} width={640} height={480} />
-        <canvas ref={noseCanvasRef} width={640} height={480} />
-        <video
-          style={{ visibility: showWebcam ? "visible" : "hidden" }}
-          ref={videoRef}
-          width={640}
-          height={480}
-          autoPlay
-        />
-      </div>
-      <div className="flex flex-col-reverse xl:flex-col justify-between w-full xl:w-1/2 xl:px-8 text-lg">
-        <div>
-          <h2 className="font-semibold text-2xl text-center mb-3">
-            Select a Color
-          </h2>
-          <div className="flex flex-wrap justify-center items-center mb-6">
-            {colors.map((color) => (
-              <button
-                key={color}
-                style={{ backgroundColor: color }}
-                className={`mr-4 mb-2 rounded-full focus:outline-none focus:shadow-outline  ${
-                  color === strokeColor ? "w-13 h-13" : "w-10 h-10"
-                }`}
-                onClick={() => setStrokeColor(color)}
-              />
-            ))}
+    <>
+      <div className="flex flex-col xl:flex-row max-w-2xl mx-auto xl:max-w-full">
+        <div className="canvas-container rounded overflow-hidden shadow-lg w-full xl:w-1/2 mb-12 xl:mb-0">
+          <div className="flex flex-col justify-center items-center">
+            {status === "loading" && (
+              <p className="font-semibold text-2xl">loading posenet...</p>
+            )}
+            {status === "error" && (
+              <>
+                <p className="font-semibold text-2xl">
+                  error loading posenet :(
+                </p>
+                <p className="text-lg">please enable your camera</p>
+              </>
+            )}
           </div>
-          <h2 className="font-semibold text-2xl text-center mb-3">
-            Select a Stroke Width
-          </h2>
-          <div className="flex flex-wrap justify-center mb-6">
-            {widths.map((width) => (
-              <button
-                key={width}
-                className="focus:outline-none group flex flex-col items-center mr-2"
-                onClick={() => setLineWidth(width)}
-              >
-                <div className="w-10 h-10 mb-4 flex flex-col items-center justify-center">
-                  <div
-                    style={{ width: width, height: width }}
-                    className="bg-gray-900 rounded-full group-focus:shadow-outline"
-                  />
-                </div>
-                <span
-                  className={`text-sm text-center border-2 rounded py-1 px-2 ${
-                    lineWidth === width
-                      ? "border-gray-600"
-                      : "border-transparent"
+          <canvas ref={drawCanvasRef} width={640} height={480} />
+          <canvas ref={noseCanvasRef} width={640} height={480} />
+          <video
+            style={{ visibility: showWebcam ? "visible" : "hidden" }}
+            ref={videoRef}
+            width={640}
+            height={480}
+            autoPlay
+          />
+        </div>
+        <div className="flex flex-col-reverse xl:flex-col justify-between w-full xl:w-1/2 xl:px-8 text-lg">
+          <div>
+            <h2 className="font-semibold text-2xl text-center mb-3">
+              Select a Color
+            </h2>
+            <div className="flex flex-wrap justify-center items-center mb-6">
+              {colors.map((color) => (
+                <button
+                  key={color}
+                  style={{ backgroundColor: color }}
+                  className={`mr-4 mb-2 rounded-full focus:outline-none focus:shadow-outline  ${
+                    color === strokeColor ? "w-13 h-13" : "w-10 h-10"
                   }`}
+                  onClick={() => setStrokeColor(color)}
+                />
+              ))}
+            </div>
+            <h2 className="font-semibold text-2xl text-center mb-3">
+              Select a Stroke Width
+            </h2>
+            <div className="flex flex-wrap justify-center mb-6">
+              {widths.map((width) => (
+                <button
+                  key={width}
+                  className="focus:outline-none group flex flex-col items-center mr-2"
+                  onClick={() => setLineWidth(width)}
                 >
-                  {width}px
-                </span>
-              </button>
-            ))}
+                  <div className="w-10 h-10 mb-4 flex flex-col items-center justify-center">
+                    <div
+                      style={{ width: width, height: width }}
+                      className="bg-gray-900 rounded-full group-focus:shadow-outline"
+                    />
+                  </div>
+                  <span
+                    className={`text-sm text-center border-2 rounded py-1 px-2 ${
+                      lineWidth === width
+                        ? "border-gray-600"
+                        : "border-transparent"
+                    }`}
+                  >
+                    {width}px
+                  </span>
+                </button>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col-reverse xl:flex-col mb-6 xl:mb-0">
+            <button
+              onClick={() => setShowWebcam((s) => !s)}
+              className="py-3 px-4 font-semibold text-center mb-2 rounded focus:outline-none focus:shadow-outline"
+            >
+              {showWebcam ? "hide" : "show"} webcam
+            </button>
+            <button
+              className="py-3 px-4 shadow-md bg-indigo-600 font-semibold text-gray-100 rounded focus:outline-none focus:shadow-outline"
+              onPointerDown={() => setPointerDown(true)}
+              onPointerUp={() => setPointerDown(false)}
+            >
+              press here to draw
+            </button>
           </div>
         </div>
-        <div className="flex flex-col-reverse xl:flex-col mb-6 xl:mb-0">
-          <button
-            onClick={() => setShowWebcam((s) => !s)}
-            className="py-3 px-4 font-semibold text-center mb-2 rounded focus:outline-none focus:shadow-outline"
-          >
-            {showWebcam ? "hide" : "show"} webcam
-          </button>
-          <button
-            className="py-3 px-4 shadow-md bg-indigo-600 font-semibold text-gray-100 rounded focus:outline-none focus:shadow-outline"
-            onPointerDown={() => setPointerDown(true)}
-            onPointerUp={() => setPointerDown(false)}
-          >
-            tap here to draw!
-          </button>
-        </div>
       </div>
-    </div>
+      <div className="text-center pt-20 pb-32">
+        <p className="text-xl">Done drawing?</p>
+        <a
+          className="inline-block text-2xl border-b-2 border-indigo-600"
+          download={"nosedraw.jpg"}
+          href={drawCanvasRef.current
+            ?.toDataURL("image/png")
+            .replace("image/png", "image/octet-stream")}
+        >
+          Click here to download your image!
+        </a>
+      </div>
+    </>
   );
 }
